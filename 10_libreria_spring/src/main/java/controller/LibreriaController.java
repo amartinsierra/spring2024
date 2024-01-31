9eb4fc028e791b4f3d81e908bf2a4c3734401fc2
Profesortarde <Profesortarde@A13E100>
2024-01-31 20:59:09 +0100

    proyecto 10 controller

---
+++ 10_libreria_spring/src/main/java/controller/LibreriaController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpSession;
+import model.Cliente;
+import model.Libro;
+import service.interfaces.ClientesService;
+import service.interfaces.LibrosService;
+
+@Controller
+public class LibreriaController {
+	@Autowired
+	LibrosService librosService;
+	@Autowired
+	ClientesService clientesService;
+	
+	@PostMapping(value="alta")
+	public String altaCliente(@ModelAttribute Cliente cliente, Model model) {
+		if(!clientesService.altaCliente(cliente)) {
+			model.addAttribute("mensaje","Usuario repetido, no se pudo registrar");
+			return "nuevo";
+		}
+		return "login";
+	}
+	@GetMapping(value="login")
+	public String login(@RequestParam("usuario") String usuario,
+			@RequestParam("password") String password, Model model) {
+		if(clientesService.autenticarCliente(usuario, password)==null) {
+			model.addAttribute("mensaje", "Usuario no existente, registrese");
+			return "login";
+		}
+		model.addAttribute("temas", librosService.getTemas());
+		return "visor";
+	}
+	
+	
+	@GetMapping(value="libros",produces="application/json")
+	public @ResponseBody List<Libro> librosTema(@RequestParam("idTema") int idTema){
+		return librosService.librosTema(idTema);
+	}
+	@GetMapping(value="agregarCarrito",produces="application/json")
+	public @ResponseBody List<Libro> agregarCarrito(@RequestParam("isbn") int isbn,HttpSession sesion){
+		
+		
+	}
+	@GetMapping(value="quitarCarrito",produces="application/json")
+	public @ResponseBody List<Libro> quitarCarrito(@RequestParam("pos") int pos,HttpSession sesion){
+		
+		
+	}
+	
+}
